1aa23b2957f64b16b9e477fe830f495d24ad8750
Dragonlord8781 <evan.stone638@gmail.com>
2025-04-02 10:01:34 -0500

    Finished

---
+++ Greetings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Stone_Evan
+{
+    class Greetings
+    {
+        //creates the public function: Welcome
+        public void Welcome()
+        {
+            //Writes line saying hello (O-si-yo)
+            Console.WriteLine("Osiyo, Welcome User!");
+        }
+
+        //creates the pubic function: Hello, also gives it the string parameter: name
+        public void Hello(string name)
+        {
+            //writes a line thanking the user for logging on using the name string parameter 
+            Console.WriteLine($"Well, {name}, thank you for logging on.");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace MyApp
+namespace Classes_Stone_Evan
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //writes program #
+            Console.WriteLine("Program 10");
+
+            //declares and initalizes the greetings object, connecting class Program to class Greetings
+            Greetings greetings = new Greetings();
+            //Calls the Welcome function
+            greetings.Welcome();
+
+            //writes a line askihng for a name
+            Console.WriteLine("Please input your name.");
+            //Declares the string userName and assignes it the inputted name
+            string userName = Console.ReadLine();
+
+            //Calls the Hello function using the userName parameter
+            greetings.Hello(userName);
         }
     }
 }
